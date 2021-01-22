a4473ae52e1d0352d7e904ffa5503fb4dca5d79d
laorange <2625835752@qq.com>
2021-01-22 14:57:10 +0800

    v2.0.0 将admin_id注册

--- file_manage/admin.py
+++ file_manage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from file_manage.models import WechatGroupFile, GroupMember, WechatFriendInfo
+from file_manage.models import WechatGroupFile, GroupMember, WechatFriendInfo, Administrator
 
 # Register your models here.
 admin.site.register(WechatGroupFile)
 admin.site.register(GroupMember)
 admin.site.register(WechatFriendInfo)
+admin.site.register(Administrator)
