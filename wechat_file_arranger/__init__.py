bd353ac0787e6dda246cf1029495674c0c85d86b
laorange <2625835752@qq.com>
2021-01-26 10:05:28 +0800

    v2.2.0 更新gitignore

---
+++ wechat_file_arranger/__init__.py
@@ -0,0 +1,19 @@
+# import pymysql
+# pymysql.install_as_MySQLdb()
+# import os
+# import django
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wechat_file_arranger.settings")
+# django.setup()
+#
+# from wechat_func import log_in_wechat
+# spy = log_in_wechat()
+
+
+# from file_manage.models import WechatGroupFile
+
+# GROUP_ID = []
+# FILE_DIR_PATH = ''
+#
+#
+# def send_file():
+#     pass
