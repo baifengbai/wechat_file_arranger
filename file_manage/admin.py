bf9b3e57ea024abe02a97fd9499fd630d9ecad65
laorange <2625835752@qq.com>
2021-01-27 10:37:10 +0800

    v2.2.2 优化admin页面的显示

--- file_manage/admin.py
+++ file_manage/admin.py
@@ -4,15 +4,23 @@ from file_manage.models import WechatGroupFile, GroupMember, WechatFriendInfo, A
 
 # Register your models here.
 class WechatGroupFileAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'remark', 'uploader_nickname', "create_time")
     readonly_fields = ("file_name", "create_time")
 
 
 class GroupMemberAdmin(admin.ModelAdmin):
+    list_display = ("wx_id", "nickname", "update_time")
     readonly_fields = ("wx_id", "create_time", "update_time")
 
 
 class WechatFriendInfoAdmin(admin.ModelAdmin):
+    list_display = ("wx_id", "nickname", "update_time")
     readonly_fields = ("wx_id", "update_time")
+
+
+class AdministratorAdmin(admin.ModelAdmin):
+    list_display = ("wx_id", "remark")
+    readonly_fields = ("update_time", )
 
 
 admin.site.register(WechatGroupFile, WechatGroupFileAdmin)
